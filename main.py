644f3ab155e907d8f8113df275bdef4560b26238
Guilherme Lopes <guiplopes29@gmail.com>
2024-12-19 10:51:31 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,31 +1,41 @@
-import discord
-from whois import whois
 import os
-from dotenv import load_dotenv
+import sys
 import asyncio
 import logging
 
+import discord
+from whois import whois
+from dotenv import load_dotenv
+
+"""
+WhoisBot: Um bot do Discord para verificar a disponibilidade de domínios.
+"""
+
+# Configurar o logger para gravar em um arquivo
 logging.basicConfig(
     level=logging.INFO,
     format='%(asctime)s %(levelname)s %(message)s',
-    handlers=[logging.FileHandler("whois_bot.log"),
-              logging.StreamHandler()])
+    handlers=[
+        logging.FileHandler("whois_bot.log"),
+        logging.StreamHandler()
+    ]
+)
 logger = logging.getLogger(__name__)
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 CHANNEL_ID = os.getenv('DISCORD_CHANNEL')
 
+# Verificar se as variáveis de ambiente foram carregadas corretamente
 if TOKEN is None or CHANNEL_ID is None:
     logger.error("Variáveis de ambiente TOKEN ou CHANNEL_ID não definidas.")
-    exit(1)
+    sys.exit(1)
 
 CHANNEL_ID = int(CHANNEL_ID)
 
 intents = discord.Intents.default()
 intents.message_content = True
 client = discord.Client(intents=intents)
-
 
 async def verificar_dominio(dominio):
     """
@@ -34,21 +44,20 @@ async def verificar_dominio(dominio):
     try:
         w = whois(dominio)
         if not w or not w.status:
-            logger.info(f"Domínio {dominio} disponível!")
+            logger.info("Domínio %s disponível!", dominio)
             return "Disponível"
 
         # Limpar os links dos status
         status_str = ", ".join([status.split(" ")[0] for status in w.status])
-        logger.info(f"Status do domínio {dominio}: {status_str}")
+        logger.info("Status do domínio %s: %s", dominio, status_str)
         return status_str
 
     except whois.parser.PywhoisError:
-        logger.info(f"Domínio {dominio} não encontrado.")
+        logger.info("Domínio %s não encontrado.", dominio)
         return "Disponível"
     except Exception as e:
-        logger.error(f"Erro ao consultar WHOIS para {dominio}: {e}")
+        logger.error("Erro ao consultar WHOIS para %s: %s", dominio, e)
         return f"Erro: {e}"
-
 
 async def loop_verificacao():
     """
@@ -58,45 +67,37 @@ async def loop_verificacao():
     channel = client.get_channel(CHANNEL_ID)
 
     if channel is None:
-        logger.error(
-            f"Canal com ID {CHANNEL_ID} não encontrado. Verifique o .env.")
+        logger.error("Canal com ID %d não encontrado. Verifique o .env.", CHANNEL_ID)
         return
 
-    dominios = ["nandomain.dev"]
+    dominios = ["solutech.dev", "nandomain.dev"]
     while True:
         for dominio in dominios:
             try:
                 status = await verificar_dominio(dominio)
                 if status:
-                    await channel.send(
-                        f"Status do domínio **{dominio}**: {status}")
+                    await channel.send(f"Status do domínio **{dominio}**: {status}")
                 else:
-                    await channel.send(
-                        f"Não foi possível verificar o domínio **{dominio}**.")
+                    await channel.send(f"Não foi possível verificar o domínio **{dominio}**.")
             except discord.errors.HTTPException as e:
-                logger.error(f"Erro ao enviar mensagem para o Discord: {e}")
+                logger.error("Erro ao enviar mensagem para o Discord: %s", e)
                 if e.code == 50035:
-                    await channel.send(
-                        f"A resposta para {dominio} é muito longa para o Discord."
-                    )
+                    await channel.send(f"A resposta para {dominio} é muito longa para o Discord.")
                 else:
-                    await channel.send(
-                        f"Ocorreu um erro ao verificar {dominio}. Tente novamente mais tarde."
-                    )
+                    await channel.send(f"Ocorreu um erro ao verificar {dominio}. Tente novamente mais tarde.")
             except Exception as e:
-                logger.error(
-                    f"Erro inesperado no loop de verificação para {dominio}: {e}"
-                )
-                await channel.send(
-                    f"Ocorreu um erro ao verificar {dominio}. Contate um administrador."
-                )
+                logger.error("Erro inesperado no loop de verificação para %s: %s", dominio, e)
+                await channel.send(f"Ocorreu um erro ao verificar {dominio}. Contate um administrador.")
 
         await asyncio.sleep(60 * 60)
-      
+
 @client.event
 async def on_ready():
-    logger.info(f'{client.user} está conectado ao Discord!')
-    logger.info(f"Conectado ao canal: {client.get_channel(CHANNEL_ID)}")
+    """
+    Evento que é acionado quando o bot está pronto para uso.
+    """
+    logger.info('%s está conectado ao Discord!', client.user)
+    logger.info("Conectado ao canal: %s", client.get_channel(CHANNEL_ID))
     client.loop.create_task(loop_verificacao())
 
 client.run(TOKEN)
